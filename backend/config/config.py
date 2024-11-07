b1615b7c2c2bc8e037471072655093d3f1e5d16c
alevar <ales.varabyou@jhu.edu>
2024-11-07 10:55:00 -0500

    cleanup old backend

--- backend/config/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE = os.path.join(BASE_DIR, '../data', 'hiv_atlas.db')
--- backend/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE genome (
-    accession_id TEXT PRIMARY KEY,
-    description TEXT,
-    fasta_file TEXT,
-    gtf_file TEXT,
-    gff_file TEXT,
-    is_reference INTEGER DEFAULT 0, -- 0 = not a reference, 1 = is a reference
-    UNIQUE (is_reference) WHERE is_reference = 1 -- Ensure only one reference
-);
